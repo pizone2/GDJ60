28bc5c4052913a4081d2ee833438e86b6ef5e3c2
pizone2 <rlawlstj0123@naver.com>
2023-01-03 14:19:01 +0900

    test

--- src/com/iu/s4/objects1/ex1/Main.java
+++ src/com/iu/s4/objects1/ex1/Main.java
@@ -2,7 +2,7 @@ package com.iu.s4.objects1.ex1;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.println("start");
 		MenuController md = new MenuController();
 		
 	//	menuController.start();
